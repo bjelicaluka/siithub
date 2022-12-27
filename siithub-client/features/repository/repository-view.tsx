de50c6f726b47693de327208082e3f8ab2856325
bjelicaluka <bjelicaluka99@gmail.com>
2022-12-27 16:34:10 +0000

    feat: #8 delete repository test: delete repo

--- siithub-client/features/repository/repository-view.tsx
+++ siithub-client/features/repository/repository-view.tsx
@@ -1,9 +1,41 @@
 import { useRouter } from "next/router";
-import { FC } from "react";
+import { type FC } from "react";
+import { Button } from "../../core/components/Button";
+import { ResultStatus, useResult } from "../../core/contexts/Result";
+import { useAction } from "../../core/hooks/useAction";
+import { useNotifications } from "../../core/hooks/useNotifications";
+import { extractErrorMessage } from "../../core/utils/errors";
+import { deleteRepository } from "./repository.service";
 
 export const RepositoryView: FC = () => {
   const router = useRouter();
-  const repository = router.query?.repository?.toString() ?? "";
+  const notifications = useNotifications();
+  const { setResult } = useResult("delete-repo");
+  const repositoryId = router.query?.repository?.toString() ?? "";
 
-  return <div>{repository}</div>;
+  const deleteRepositoryAction = useAction<string>(
+    (id) => deleteRepository(id),
+    {
+      onSuccess: () => {
+        notifications.success(
+          "You have successfully deleted a new repository."
+        );
+        setResult({ status: ResultStatus.Ok, type: "CREATE_REPO" });
+        router.push("/");
+      },
+      onError: (error: any) => {
+        notifications.error(extractErrorMessage(error));
+        setResult({ status: ResultStatus.Error, type: "CREATE_REPO" });
+      },
+    }
+  );
+
+  return (
+    <div>
+      {repositoryId}
+      <Button onClick={() => deleteRepositoryAction(repositoryId)}>
+        Delete
+      </Button>
+    </div>
+  );
 };
